e0d0dd78dd29f1e2b54244da3b13c4aa4a60fedd
PhenoM4n4n <phenom4n4n@gmail.com>
2021-08-16 23:36:38 -0700

    add unix block support

--- TagScriptEngine/block/strf.py
+++ TagScriptEngine/block/strf.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from typing import Optional
 
 from ..interface import verb_required_block
@@ -11,7 +11,11 @@ class StrfBlock(verb_required_block(True, payload=True)):
     Two types of timestamps are supported: ISO and epoch.
     If a timestamp isn't passed, the current UTC time is used.
 
+    Invoking this block with `unix` will return the current Unix timestamp.
+
     **Usage:** ``{strf([timestamp]):<format>}``
+
+    **Aliases:** ``unix``
 
     **Payload:** format, None
 
@@ -30,11 +34,16 @@ class StrfBlock(verb_required_block(True, payload=True)):
 
         {strf(2019-10-09T01:45:00.805000):%H:%M %d-%B-%Y}
         # 01:45 09-October-2019
+
+        {unix}
+        # 1629182008
     """
 
-    ACCEPTED_NAMES = ("strf",)
+    ACCEPTED_NAMES = ("strf", "unix")
 
     def process(self, ctx: Context) -> Optional[str]:
+        if ctx.verb.declaration.lower() == "unix":
+            return str(int(datetime.now(timezone.utc).timestamp()))
         if ctx.verb.parameter:
             if ctx.verb.parameter.isdigit():
                 try:
@@ -48,5 +57,7 @@ class StrfBlock(verb_required_block(True, payload=True)):
                 except ValueError:
                     return
         else:
-            t = datetime.utcnow()
+            t = datetime.now()
+        if not t.tzinfo:
+            t = t.replace(tzinfo=timezone)
         return t.strftime(ctx.verb.payload)
